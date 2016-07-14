3fbe230369e879b800261f96a7d2e1f05ca0cb1f
AlexeyPogorelov <hirurg555@gmail.com>
2016-07-14 14:02:01 +0300

    collapse

--- src/js/pogorelov.js
+++ src/js/pogorelov.js
@@ -1048,6 +1048,124 @@ $.fn.scrollSlider = function (opt) {
 
 };
 
+// Collapse
+
+$.fn.collapse = function (opt) {
+
+	// options
+	if (typeof opt !== 'object') opt = {};
+
+	opt = $.extend({
+		'opened': false,
+		'panelClass': 'panel',
+		'triggerClass': 'panel-heading',
+		'collapseClass': 'panel-collapse'
+	}, opt);
+
+
+	var plugin = function (i) {
+		var $self = $(this),
+			DOM = {},
+			state = {},
+			$window = $(window);
+		var plg = {
+			init: function () {
+				if (!state.initialized) {
+					this.cacheDOM();
+					this.bindEvents();
+					state.initialized = true;
+				}
+				this.resize();
+				if (typeof opt.opened === 'number') {
+					this.hideAll();
+					this.show(DOM.$panels.eq(opt.opened), DOM.collapses[opt.opened], opt.opened);
+				} else {
+					state.opened = null;
+					this.hideAll();
+				}
+			},
+			cacheDOM: function () {
+				DOM.$panels = $self.find('.' + opt.panelClass);
+				DOM.collapses = [];
+				DOM.triggers = [];
+				DOM.$panels.each(function () {
+					var $self = $(this);
+					DOM.collapses.push( $self.find('.' + opt.collapseClass) );
+					DOM.triggers.push( $self.find('.' + opt.triggerClass) );
+				});
+			},
+			bindEvents: function () {
+				DOM.$panels.each(function (i) {
+					var $self = $(this),
+						$trigger = DOM.triggers[i],
+						$collapse = DOM.collapses[i];
+					$trigger
+						.on('click', function (e) {
+							e.preventDefault();
+							plg.clickTrigger(
+								$self,
+								$collapse,
+								i
+							);
+						});
+				});
+				$window.on('resize', this.resize);
+			},
+			show: function ($panel, $collapse, i) {
+				if (typeof state.opened === 'number') {
+					this.hide(DOM.$panels.eq(state.opened), DOM.collapses[state.opened], state.opened);
+				}
+
+				state.opened = i;
+				$panel.removeClass('collapsed');
+				// console.log( $collapse.data('fullHeight') )
+				$collapse.css('height', $collapse.data('fullHeight'));
+			},
+			hide: function ($panel, $collapse, i) {
+				if (i === state.opened) state.opened = null;
+				// console.log( $collapse )
+				$panel.addClass('collapsed');
+				$collapse.css('height', '');
+			},
+			clickTrigger: function ($panel, $collapse, i) {
+				if ($panel.hasClass('collapsed')) {
+					this.show( $panel, $collapse, i );
+				} else {
+					this.hide( $panel, $collapse, i );
+				}
+
+			},
+			hideAll: function () {
+				DOM.$panels.each(function (i) {
+					this.hide(DOM.$panels.eq(i), DOM.collapses[i], i);
+				}.bind(this));
+			},
+			resize: function () {
+				for (var i = 0; i < DOM.collapses.length; i++) {
+					DOM.collapses[i].data(
+						'fullHeight',
+						DOM.collapses[i].prop('scrollHeight')
+					);
+				}
+			}
+		};
+
+		plg.init();
+
+		return plg;
+	};
+
+	if (this.length > 1) {
+
+		return this.each(plugin);
+
+	} else if (this.length === 1) {
+
+		return plugin.call(this[0]);
+
+	}
+};
+
 $.fn.validate = function (opt) {
 
 	this.each(function (i) {
